45e64d10683630344cbe4906b62a988f02056d5b
Atsuya Tanaka <tenmusu007@gmail.com>
2022-10-26 11:10:54 -0700

    create matching user list

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -2,9 +2,8 @@ const User = require("../models/Users");
 const bcrypt = require("bcrypt");
 const CreateUser = async (req, res) => {
 	try {
-		console.log(req.body);
-    const checkEmail = await User.findOne({ email: req.body.email });
-    if (checkEmail) return res.json("exsist");
+		const checkEmail = await User.findOne({ email: req.body.email });
+		if (checkEmail) return res.json("exsist");
 		const hashPsw = await bcrypt
 			.hash(req.body.password, 12)
 			.then((hashedPassword) => {
@@ -14,18 +13,8 @@ const CreateUser = async (req, res) => {
 			email: req.body.email,
 			password: hashPsw,
 		});
-		// const addIdUser = await {
-		// 	// ...newUser,
-		// 	...newUser._doc,
-		// 	user_id: newUser._id.toString(),
-		// };
-		// console.log("add", addIdUser);
-		// const user = await addIdUser.save();
 		const user = await newUser.save();
-
-		// console.log("user",user);
-		// console.log("add",user);
-		res.status(200).json({ ...user._doc, user_id:user._id });
+		res.status(200).json({ ...user._doc, user_id: user._id });
 	} catch (err) {
 		res.status(500).json(err);
 	}
@@ -35,11 +24,38 @@ const LoginUser = async (req, res) => {
 	try {
 		const user = await User.findOne({ email: req.body.email });
 		if (!user) return res.status(404).send("We can not find the user");
-		const isUserMatch = await bcrypt.compare(req.body.password, user.password)
+		const isUserMatch = await bcrypt.compare(req.body.password, user.password);
 		if (!isUserMatch) return res.status(400).json("password is wrong");
 		return res.status(200).json(user);
 	} catch (err) {
-		res.status(500).json(err)
+		res.status(500).json(err);
 	}
-}
-module.exports = { CreateUser, LoginUser };
+};
+
+const AllSet = async (req, res) => {
+	try {
+		const checkEmail = await User.findOne({ email: req.body.email });
+		if (checkEmail) return res.json("exsist");
+		const hashPsw = await bcrypt
+			.hash(req.body.password, 12)
+			.then((hashedPassword) => {
+				return hashedPassword;
+			});
+		const newUser = await new User({
+			email: req.body.email,
+			password: hashPsw,
+			username: req.body.username,
+			course: req.body.course,
+			sexual_orientation: req.body.sexual_orientation,
+			age: req.body.age,
+			about: req.body.about,
+			gender: req.body.gender,
+			interests: req.body.interests,
+		});
+		const user = await newUser.save();
+		res.status(200).json({ ...user._doc, user_id: user._id });
+	} catch (err) {
+		console.log(err);
+	}
+};
+module.exports = { CreateUser, LoginUser, AllSet };
--- backend/controllers/likes.js
+++ backend/controllers/likes.js
@@ -7,8 +7,13 @@ const sendLike = async (req, res) => {
 			to: req.body.to,
 		});
 		const like = await newLike.save();
-		console.log(like);
-		res.status(200).json(like);
+		const checkLike = await Like.find({ from: req.body.to });
+		if (checkLike.length === 0) {
+			return res.status(200).json(like);
+		} else {
+			const userInfo = await User.findById(checkLike[0].from);
+			res.status(200).json(userInfo);
+		}
 	} catch (err) {
 		res.status(500).json(err);
 	}
@@ -17,25 +22,10 @@ const getLike = async (req, res) => {
 	try {
 		const likeList = await Like.find({ to: req.body.uesr_id });
 		const likedUser = [];
-		// const getEachUser = async() => {
-		//   likeList.forEach(async (item) => {
-		//     // if(likedUser.length === 2 ) return res.status(200).json(likedUser);
-		//     const userSendLike = await User.findById(item.from)
-		//     console.log("user", userSendLike);
-		//     likedUser.push(userSendLike);
-		//     console.log("list", likedUser);
-		//   })
-		// }
 		for (const item of likeList) {
 			const userSendLike = await User.findById(item.from);
 			likedUser.push(userSendLike);
 		}
-		// await getEachUser();
-		// const getEachUser = likeList.map(async(item) => {
-		//   const userSendLike = await User.findById(item.from)
-		//   likedUser.push(userSendLike);
-		//   return userSendLike
-		// })
 		res.status(200).json(likedUser);
 	} catch (err) {
 		res.status(500).json(err);
@@ -43,8 +33,31 @@ const getLike = async (req, res) => {
 };
 const getUsers = async (req, res) => {
 	try {
-		const userList = await User.find({ gender: req.body.sexual_orientation });
-		res.status(200).json(userList);
+		const List = await User.find();
+		console.log("List", List);
+		const whoLike = await List.filter((item) => {
+			for (const element of req.body.sexual_orientation) {
+				console.log("ele",element);
+				if (item.gender === element) {
+					console.log("FOR item", item);
+					return item;
+				}
+			}
+		});
+		if (req.body.sexual_orientation.length > 1) {
+			const filterdLike = await whoLike.filter((item) => {
+				for (const element of item.sexual_orientation) {
+					if (element === req.body.gender) {
+						return item;
+					}
+				}
+			});
+			console.log("filter", filterdLike);
+			const delCurrentUser =filterdLike.filter((item)=>item._id.toString() !== req.body.uesr_id)
+			res.status(200).json(delCurrentUser);
+		} else {
+			res.status(200).json(whoLike);
+		}
 	} catch (err) {
 		res.status(500).json(err);
 	}
