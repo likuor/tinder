d7c49298260c41bb4f8bac397ec3817220223f7c
Koki Sakai <likuor0528@gmail.com>
2022-10-26 17:15:33 -0700

    Add useRef on profile

--- frontend/src/components/BasicModal.js
+++ frontend/src/components/BasicModal.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import PropTypes from 'prop-types';
 import Button from '@mui/material/Button';
 import { styled } from '@mui/material/styles';
@@ -11,136 +11,10 @@ import CloseIcon from '@mui/icons-material/Close';
 import TextField from '@mui/material/TextField';
 import MenuItem from '@mui/material/MenuItem';
 import Box from '@mui/material/Box';
-import Interests from './Interests';
 import Autocomplete from '@mui/material/Autocomplete';
 
-const top100Films = [
-  { title: 'The Shawshank Redemption', year: 1994 },
-  { title: 'The Godfather', year: 1972 },
-  { title: 'The Godfather: Part II', year: 1974 },
-  { title: 'The Dark Knight', year: 2008 },
-  { title: '12 Angry Men', year: 1957 },
-  { title: "Schindler's List", year: 1993 },
-  { title: 'Pulp Fiction', year: 1994 },
-  {
-    title: 'The Lord of the Rings: The Return of the King',
-    year: 2003,
-  },
-  { title: 'The Good, the Bad and the Ugly', year: 1966 },
-  { title: 'Fight Club', year: 1999 },
-  {
-    title: 'The Lord of the Rings: The Fellowship of the Ring',
-    year: 2001,
-  },
-  {
-    title: 'Star Wars: Episode V - The Empire Strikes Back',
-    year: 1980,
-  },
-  { title: 'Forrest Gump', year: 1994 },
-  { title: 'Inception', year: 2010 },
-  {
-    title: 'The Lord of the Rings: The Two Towers',
-    year: 2002,
-  },
-  { title: "One Flew Over the Cuckoo's Nest", year: 1975 },
-  { title: 'Goodfellas', year: 1990 },
-  { title: 'The Matrix', year: 1999 },
-  { title: 'Seven Samurai', year: 1954 },
-  {
-    title: 'Star Wars: Episode IV - A New Hope',
-    year: 1977,
-  },
-  { title: 'City of God', year: 2002 },
-  { title: 'Se7en', year: 1995 },
-  { title: 'The Silence of the Lambs', year: 1991 },
-  { title: "It's a Wonderful Life", year: 1946 },
-  { title: 'Life Is Beautiful', year: 1997 },
-  { title: 'The Usual Suspects', year: 1995 },
-  { title: 'Léon: The Professional', year: 1994 },
-  { title: 'Spirited Away', year: 2001 },
-  { title: 'Saving Private Ryan', year: 1998 },
-  { title: 'Once Upon a Time in the West', year: 1968 },
-  { title: 'American History X', year: 1998 },
-  { title: 'Interstellar', year: 2014 },
-  { title: 'Casablanca', year: 1942 },
-  { title: 'City Lights', year: 1931 },
-  { title: 'Psycho', year: 1960 },
-  { title: 'The Green Mile', year: 1999 },
-  { title: 'The Intouchables', year: 2011 },
-  { title: 'Modern Times', year: 1936 },
-  { title: 'Raiders of the Lost Ark', year: 1981 },
-  { title: 'Rear Window', year: 1954 },
-  { title: 'The Pianist', year: 2002 },
-  { title: 'The Departed', year: 2006 },
-  { title: 'Terminator 2: Judgment Day', year: 1991 },
-  { title: 'Back to the Future', year: 1985 },
-  { title: 'Whiplash', year: 2014 },
-  { title: 'Gladiator', year: 2000 },
-  { title: 'Memento', year: 2000 },
-  { title: 'The Prestige', year: 2006 },
-  { title: 'The Lion King', year: 1994 },
-  { title: 'Apocalypse Now', year: 1979 },
-  { title: 'Alien', year: 1979 },
-  { title: 'Sunset Boulevard', year: 1950 },
-  {
-    title:
-      'Dr. Strangelove or: How I Learned to Stop Worrying and Love the Bomb',
-    year: 1964,
-  },
-  { title: 'The Great Dictator', year: 1940 },
-  { title: 'Cinema Paradiso', year: 1988 },
-  { title: 'The Lives of Others', year: 2006 },
-  { title: 'Grave of the Fireflies', year: 1988 },
-  { title: 'Paths of Glory', year: 1957 },
-  { title: 'Django Unchained', year: 2012 },
-  { title: 'The Shining', year: 1980 },
-  { title: 'WALL·E', year: 2008 },
-  { title: 'American Beauty', year: 1999 },
-  { title: 'The Dark Knight Rises', year: 2012 },
-  { title: 'Princess Mononoke', year: 1997 },
-  { title: 'Aliens', year: 1986 },
-  { title: 'Oldboy', year: 2003 },
-  { title: 'Once Upon a Time in America', year: 1984 },
-  { title: 'Witness for the Prosecution', year: 1957 },
-  { title: 'Das Boot', year: 1981 },
-  { title: 'Citizen Kane', year: 1941 },
-  { title: 'North by Northwest', year: 1959 },
-  { title: 'Vertigo', year: 1958 },
-  {
-    title: 'Star Wars: Episode VI - Return of the Jedi',
-    year: 1983,
-  },
-  { title: 'Reservoir Dogs', year: 1992 },
-  { title: 'Braveheart', year: 1995 },
-  { title: 'M', year: 1931 },
-  { title: 'Requiem for a Dream', year: 2000 },
-  { title: 'Amélie', year: 2001 },
-  { title: 'A Clockwork Orange', year: 1971 },
-  { title: 'Like Stars on Earth', year: 2007 },
-  { title: 'Taxi Driver', year: 1976 },
-  { title: 'Lawrence of Arabia', year: 1962 },
-  { title: 'Double Indemnity', year: 1944 },
-  {
-    title: 'Eternal Sunshine of the Spotless Mind',
-    year: 2004,
-  },
-  { title: 'Amadeus', year: 1984 },
-  { title: 'To Kill a Mockingbird', year: 1962 },
-  { title: 'Toy Story 3', year: 2010 },
-  { title: 'Logan', year: 2017 },
-  { title: 'Full Metal Jacket', year: 1987 },
-  { title: 'Dangal', year: 2016 },
-  { title: 'The Sting', year: 1973 },
-  { title: '2001: A Space Odyssey', year: 1968 },
-  { title: "Singin' in the Rain", year: 1952 },
-  { title: 'Toy Story', year: 1995 },
-  { title: 'Bicycle Thieves', year: 1948 },
-  { title: 'The Kid', year: 1921 },
-  { title: 'Inglourious Basterds', year: 2009 },
-  { title: 'Snatch', year: 2000 },
-  { title: '3 Idiots', year: 2009 },
-  { title: 'Monty Python and the Holy Grail', year: 1975 },
-];
+////////////////////////// dummy //////////////////////////
+const top100Films = [{ hobby: 'coffee' }, { hobby: 'The Godfather' }];
 
 const BootstrapDialog = styled(Dialog)(({ theme }) => ({
   '& .MuiDialogContent-root': {
@@ -184,33 +58,34 @@ const courses = [
 
 const genders = [
   {
-    value: 'male',
+    value: 1,
     label: 'Male',
   },
   {
-    value: 'female',
+    value: 2,
     label: 'Female',
   },
   {
-    value: 'X',
+    value: 3,
     label: 'X',
   },
 ];
 
-const sexualOrientation = [
+const sexualOrientations = [
   {
-    value: 'male',
+    value: 1,
     label: 'Male',
   },
   {
-    value: 'female',
+    value: 2,
     label: 'Female',
   },
   {
-    value: 'X',
+    value: 3,
     label: 'X',
   },
 ];
+////////////////////////// dummy //////////////////////////
 
 function BootstrapDialogTitle(props) {
   const { children, onClose, ...other } = props;
@@ -243,10 +118,24 @@ BootstrapDialogTitle.propTypes = {
 
 export default function BasicModal(props) {
   const { open, setOpen } = props;
-  const [openInt, setOpenInt] = React.useState(false);
   const [course, setCourse] = useState('CRS');
+  const [gender, setGender] = useState(1);
+  const [about, setAbout] = useState('');
+
   const [selectedImage, setSelectedImage] = useState(null);
   const [imageUrl, setImageUrl] = useState(null);
+
+  const [interests, setInterests] = React.useState([]);
+  const [inputInterestsVal, setInputInterestsVal] = React.useState('');
+
+  const [sexualOri, setSexualOri] = React.useState([]);
+  const [inputSexualOriVal, setInputSexualOriVal] = React.useState('');
+
+  const aboutRef = useRef(null);
+  const courseRef = useRef(null);
+  const genderRef = useRef(null);
+  // const interestsRef = useRef(null);
+  // const sexualOrientationRef = useRef(null);
 
   useEffect(() => {
     if (selectedImage) {
@@ -256,20 +145,33 @@ export default function BasicModal(props) {
 
   const handleClose = () => {
     setOpen(false);
+    setAbout(aboutRef.current.value);
+
+    const userInfo = {
+      image: imageUrl,
+      course: courseRef.current.value,
+      about: aboutRef.current.value,
+      interests: interests,
+      gender: genderRef.current.value,
+      sexualOrientation: sexualOri,
+    };
+
+    console.log(userInfo);
   };
-  const handleCourse = (event) => {
-    setCourse(event.target.value);
+
+  const handleState = (event, setState) => {
+    setState(event.target.value);
   };
 
   return (
     <>
       <BootstrapDialog
         onClose={handleClose}
-        aria-labelledby='customized-dialog-title'
+        aria-labelledby='customized-dialog-hobby'
         open={open}
       >
         <BootstrapDialogTitle
-          id='customized-dialog-title'
+          id='customized-dialog-hobby'
           onClose={handleClose}
           sx={{
             width: 310,
@@ -278,6 +180,7 @@ export default function BasicModal(props) {
           Edit
         </BootstrapDialogTitle>
         <DialogContent dividers>
+          {/* image */}
           <Box
             sx={{
               display: 'flex',
@@ -307,17 +210,17 @@ export default function BasicModal(props) {
               display: 'flex',
               flexDirection: 'column',
               mx: 'auto',
-              my: 1,
+              my: 3,
             }}
           >
             <TextField
               id='standard-multiline-static'
+              inputRef={aboutRef}
               label='About me'
               multiline
               rows={10}
-              defaultValue='Aenean lacinia bibendum nulla sed consectetur. Praesent commodo
-            cursus magna, vel scelerisque nisl consectetur et. Donec sed odio
-            dui. Donec ullamcorper nulla non metus auctor fringilla.'
+              defaultValue={about}
+              placeholder='Tell us about yourself'
               variant='standard'
             />
           </Box>
@@ -328,19 +231,28 @@ export default function BasicModal(props) {
               display: 'flex',
               flexDirection: 'column',
               mx: 'auto',
-              my: 1,
+              my: 3,
             }}
           >
             <Autocomplete
               multiple
               sx={{ width: 260 }}
               limitTags={2}
+              name='movies'
               id='multiple-limit-tags'
               options={top100Films}
-              getOptionLabel={(option) => option.title}
-              defaultValue={[]}
+              getOptionLabel={(option) => option.hobby}
+              value={interests}
+              onChange={(event, newValue) => {
+                setInterests(newValue);
+              }}
+              inputValue={inputInterestsVal}
+              onInputChange={(event, newInputValue) => {
+                setInputInterestsVal(newInputValue);
+              }}
               renderInput={(params) => (
                 <TextField
+                  name='auto-input'
                   {...params}
                   label='Select your interests'
                   placeholder='Interests'
@@ -348,18 +260,6 @@ export default function BasicModal(props) {
               )}
             />
           </Box>
-
-          {/* Interests  */}
-          {/* <Box
-            sx={{
-              display: 'flex',
-              flexDirection: 'column',
-              mx: 'auto',
-              my: 1,
-            }}
-          >
-            <Interests openInt={openInt} setOpenInt={setOpenInt} />
-          </Box> */}
 
           {/* courses */}
           <Box
@@ -375,7 +275,8 @@ export default function BasicModal(props) {
               select
               label='Course'
               value={course}
-              onChange={handleCourse}
+              inputRef={courseRef}
+              onChange={(e) => handleState(e, setCourse)}
             >
               {courses.map((option) => (
                 <MenuItem key={option.value} value={option.value}>
@@ -397,8 +298,10 @@ export default function BasicModal(props) {
             <TextField
               id='outlined-select-currency'
               select
+              inputRef={genderRef}
               label='Gender'
-              value={'male'}
+              value={gender}
+              onChange={(e) => handleState(e, setGender)}
             >
               {genders.map((option) => (
                 <MenuItem key={option.value} value={option.value}>
@@ -408,7 +311,6 @@ export default function BasicModal(props) {
             </TextField>
           </Box>
 
-          {/* sexual */}
           <Box
             sx={{
               display: 'flex',
@@ -417,18 +319,31 @@ export default function BasicModal(props) {
               my: 3,
             }}
           >
-            <TextField
-              id='outlined-select-currency'
-              select
-              label='Sex Orientation'
-              value={'male'}
-            >
-              {sexualOrientation.map((option) => (
-                <MenuItem key={option.value} value={option.value}>
-                  {option.label}
-                </MenuItem>
-              ))}
-            </TextField>
+            <Autocomplete
+              multiple
+              sx={{ width: 260 }}
+              limitTags={2}
+              name='sexual'
+              id='multiple-limit-tags'
+              options={sexualOrientations}
+              getOptionLabel={(option) => option.label}
+              value={sexualOri}
+              onChange={(event, newValue) => {
+                setSexualOri(newValue);
+              }}
+              inputValue={inputSexualOriVal}
+              onInputChange={(event, newInputValue) => {
+                setInputSexualOriVal(newInputValue);
+              }}
+              renderInput={(params) => (
+                <TextField
+                  name='auto-input'
+                  {...params}
+                  label='Select your Sexual Orientation'
+                  placeholder='SexualOrientation'
+                />
+              )}
+            />
           </Box>
         </DialogContent>
         <DialogActions>
