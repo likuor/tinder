2b986fbb3f3eff957e9472e9620e39527796b27d
Atsuya Tanaka <tenmusu007@gmail.com>
2022-11-15 08:25:54 -0800

    fix post to get

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -8,7 +8,7 @@ const {
 } = require('../controllers/auth');
 router.post('/signup', CreateUser);
 router.post('/login', LoginUser);
-router.post("/logout", Logout);
+router.get("/logout", Logout);
 router.get('/getuserinfo', GetUser);
 router.get('/cookie', cookieCheck);
 module.exports = router;
