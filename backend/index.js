b48d18d19539b3d4a2065b0bd18a09b55148d222
Atsuya Tanaka <tenmusu007@gmail.com>
2022-11-16 13:27:19 -0800

    change port

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,7 @@ const cookieSession = require("cookie-session");
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 require("dotenv").config();
-const port = process.env.SERVER_URL || 8000;
+const port = 8000;
 
 const http = require("http");
 app.use(
