16b94dd38ca75d824dcc142c6a127ee1e31db58d
Atsuya Tanaka <tenmusu007@gmail.com>
2022-11-21 15:10:11 -0800

    add log at backend auth

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -28,16 +28,17 @@ const CreateUser = async (req, res) => {
 
 const LoginUser = async (req, res) => {
   try {
+    console.log("body",req.body);
     const user = await User.findOne({ email: req.body.email });
     if (!user) return res.status(404).send('We can not find the user');
     const isUserMatch = await bcrypt.compare(req.body.password, user.password);
     if (!isUserMatch) {
       return res.status(400).json('password is wrong');
     } else {
-      req.session.id = user._id.toString();
-      console.log("req", req.session.id);
-      // res.cookie('id', req.session.id);
-      return res.status(200).json({user:null});
+      // req.session.id = user._id.toString();
+      console.log("req", user);
+      // // res.cookie('id', req.session.id);
+      return res.status(200).json(user);
     }
   } catch (err) {
     res.status(500).json(err);
